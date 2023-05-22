05ce60088c22eed8ff35b422c6f38bb8ef210a62
Anvar Abdulsatarov <anvarabdulsatarov@gmail.com>
2023-05-22 19:12:02 +0500

    feat: gql generator

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,18 @@
 import { Module } from '@nestjs/common';
 import { SamplesModule } from '../samples/samples.module';
 import { ConfigModule } from '@nestjs/config';
-import { isTestEnvironment } from '../common/utils/environment';
+import { isTestEnvironment } from '@common/utils/environment';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: !isTestEnvironment() ? '.env' : '.env.test',
+    }),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
     }),
     SamplesModule,
   ],
