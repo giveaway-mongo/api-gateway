325d96b6fe0045c94dd058354f91d26c0ea81b0f
Anvar Abdulsatarov <anvarabdulsatarov@gmail.com>
2023-07-03 16:56:52 +0500

    no url

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -24,7 +24,7 @@ const protoFiles = [
             path.join(process.cwd(), 'protos'),
             protoFiles,
           ),
-          url: 'auth-service',
+          // url: 'auth-service',
         },
       },
     ]),
